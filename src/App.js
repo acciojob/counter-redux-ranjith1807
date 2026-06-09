3705e22161faf71d9a1ae17551f5a9dbcb20ba5a
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-06-09 18:05:21 +0530

    dd

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
+
+function App() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
+    </div>
+  );
+}
+
+export default App;
